8c117cc4ea14b979e5c735bd8195cf312c35a052
Jatin Nagpal <nagpaljatin1411@gmail.com>
2019-08-16 11:19:56 +0530

    added Snippet

---
+++ snippet.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define ff first
+#define ss second
+using namespace std;
+#define MP make_pair
+#define PB push_back
+#define int long long
+#define f(i,x,n) for(int i=x;i<n;i++)
+#define ld long double
+const int mod=1000000007;
+void fastio(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+
+
+int32_t main()
+{
+	fastio();
+	
+	return 0;
+}
