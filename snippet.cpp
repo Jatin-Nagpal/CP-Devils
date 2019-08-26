69564230dafa960531f0a619539ee388ce434794
coderanant <coderanant@gmail.com>
2019-08-26 18:25:07 +0530

    Updated Readme

--- snippet.cpp
+++ snippet.cpp
@@ -11,17 +11,12 @@ using namespace __gnu_pbds;
 #define f(i,x,n) for(int i=x;i<n;i++)
 #define ld long double
 #define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define CPDevils ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 const int mod=1000000007;
-void fastio(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-}
-
 
 int32_t main()
 {
-	fastio();
+	CPDevils
 	
 	return 0;
 }
