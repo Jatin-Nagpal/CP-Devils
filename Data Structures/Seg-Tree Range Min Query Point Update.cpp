1b41ce83d9b6c2be4da5c8f55fc0fb54f9da5ad3
Jatin-Nagpal <nagpaljatin1411@gmail.com>
2019-09-01 12:58:11 +0530

    Seg-Tree Added

---
+++ Data Structures/Seg-Tree Range Min Query Point Update.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define f(i,x,n) for(int i=x;i<n;i++)
+#define mod 100000007
+
+// Range Minimum Query and Point Update value
+#define N 200000 //Check the limits dummy
+int bit[4*N+10];
+int func(int a,int b)
+{
+	return min(a,b); //Check if u don't wanna find min
+}
+void init()
+{
+	f(i,0,4*N+5)
+		bit[i]=mod; //Check this carefully
+}
+int upd(int i,int j, int index,int val,int node)
+{
+	if(i==j&&index!=i)
+		return bit[node];
+	if(i==j)
+		return bit[node]=val;
+	else if(i<=index&&j>=index)
+		return bit[node]=func(upd(i,(i+j)/2,index,val,2*node), upd((i+j)/2+1,j,index,val,2*node+1) );
+	return bit[node];
+}
+int qry(int i,int j,int l,int r,int node)
+{
+	if(i==j)
+	{
+		if(l<=i&&r>=j)
+			return bit[node];
+		else
+			return mod;
+	}
+	else if(l>j||r<i)
+		return mod;
+	else if(l<=i&&r>=j)
+		return bit[node];
+	int mid=(i+j)/2;
+	return func(qry(i,mid,l,r,2*node),qry(mid+1,j,l,r,2*node+1));
+}
