5be278cc577d9ea3ee9b1c59ab657513642d9d6e
Jatin Nagpal <nagpaljatin1411@gmail.com>
2020-10-17 14:38:56 +0530

    updated snippet

---
+++ snippet2.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define ff first
+#define ss second
+#define pb push_back
+#define int long long
+#define ld long double
+#define f(i,x,n) for(int i=x;i<n;i++)
+using namespace std;
+const int mod=1e9+7;
+const int inf=1e18;
+
+void solve() {
+	
+}
+
+int32_t main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	int t=1;
+	// cin>>t;
+	while(t--) {
+		solve();
+	}
+	return 0;
+}
